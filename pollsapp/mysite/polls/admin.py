609a465aac7642bf78e6c67befcb13ef1640d79f
avin99 <avin.rathore@gmail.com>
2019-09-05 13:19:19 +0530

    filter and search box added

--- pollsapp/mysite/polls/admin.py
+++ pollsapp/mysite/polls/admin.py
@@ -9,6 +9,8 @@ class ChoiceInline(admin.StackedInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
