ea6e8dc2337f1c169bcdb4600839f7320dee63ca
avin99 <avin.rathore@gmail.com>
2019-09-12 16:43:19 +0530

    separate register page created

--- foodapp/mysite/main/views.py
+++ foodapp/mysite/main/views.py
@@ -34,6 +34,29 @@ def register(request):
     return render(request = request,
                   template_name = "main/register.html",
                   context={"form":form}) 
+def register_as_customer(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "main/register_as_customer.html",
+                          context={"form":form})
+
+            
+    form = NewUserForm
+    return render(request = request,
+                  template_name = "main/register_as_customer.html",
+                  context={"form":form}) 
 def logout_request(request):
     logout(request)
     messages.info(request, "Logged out successfully!")
