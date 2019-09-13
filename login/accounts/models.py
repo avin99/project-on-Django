9c31441c25e39291d3b5853c8683279f44a0e77d
ritik09 <ritikgupta0912@gmail.com>
2019-09-13 14:11:27 +0530

    using abstract user in signup form

--- login/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class post(models.Model):
-    name=models.CharField(max_length=100)
-    des=models.TextField()
-    price=models.IntegerField()
-    img=models.ImageField(upload_to='pics')
---
+++ mysite/main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from .forms import SignUpForm
+
+# Create your views here.
+
+def indexView(request):
+    
+    return render(request,"index.html")
+
+@login_required
+def dashboardView(request):
+    return render(request,'dashboard.html')
+
+def registerView(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+            
+
+    else:
+        form=SignUpForm()
+    return render(request,'registration/register.html',{'form':form})
