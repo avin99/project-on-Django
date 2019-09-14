3ba2b1775b6169aad34d0d17ab18578a52cce5f3
ritik09 <ritikgupta0912@gmail.com>
2019-09-14 11:37:58 +0530

    implemented abstract user

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -4,15 +4,20 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
+from .models import *
 
-User=get_user_model()
+# User=get_user_model()
 
 class SignUpForm(forms.ModelForm):
     first_name=forms.CharField(max_length=30,required=False,help_text='Optional. ')
     last_name=forms.CharField(max_length=30,required=False,help_text='Optional.')
     email=forms.EmailField(max_length=254,help_text='Required. Inform a valid email address.')
-    customer=forms.BooleanField()
-    owner=forms.BooleanField()
+    is_owner=forms.BooleanField()
+    
+    # def save(self, commit=True):
+    #     owner = self.cleaned_data.get('owner', None)
+       
+    #     return super(SignUpForm, self).save(commit=commit)
     class Meta:
         model = User
-        fields=('username','first_name','last_name','email','password','customer','owner') 
+        fields=('username','first_name','last_name','email','password','is_owner') 
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_owner=models.BooleanField(default=False)
-    is_cusomer=models.BooleanField(default=False)
+
